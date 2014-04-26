3c92a8bb3655bc4cce54b09448db878fc3cfcf17
Zach Latta <zach@zachlatta.com>
2014-04-26 00:10:56 -0700

    Walk around and stuff.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,8 +1,10 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxGroup;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
@@ -19,13 +21,19 @@ class PlayState extends FlxState
 
 	private var _playerSpeed:Float = 120;
 	private var _playerJump:Float = 200;
-	private var _flagWalking:Bool = false;
+
+	private var _ground:FlxObject;
+	private var _leftWall:FlxObject;
+	private var _rightWall:FlxObject;
+	private var _bounds:FlxGroup;
 
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
+		super.create();
+
 		FlxG.mouse.visible = false;
 		FlxG.cameras.flash(FlxColor.BLACK);
 
@@ -37,8 +45,12 @@ class PlayState extends FlxState
 		_player.loadGraphic("assets/images/player.png", true, true, 71, 132);
 		_player.animation.add("idle", [0, 1], 4, true);
 		_player.x = 284;
-		_player.y = 300;
+		_player.y = 200;
 		add(_player);
+
+		_player.acceleration.y = 420;
+		_player.maxVelocity.set(_playerSpeed, 420);
+		_player.drag.set(_playerSpeed * 4, _playerSpeed * 4);
 
 		_player.animation.play("idle");
 
@@ -48,7 +60,22 @@ class PlayState extends FlxState
 		_surface.y = 25;
 		add(_surface);
 
-		super.create();
+		_bounds = new FlxGroup();
+		_ground = new FlxObject(0, FlxG.height, FlxG.width, 30);
+		_leftWall = new FlxObject(-30, 0, 30, FlxG.height);
+		_rightWall = new FlxObject(FlxG.width, 0, 30, FlxG.height);
+
+		_ground.immovable = true;
+		_leftWall.immovable = true;
+		_rightWall.immovable = true;
+
+		_bounds.add(_ground);
+		_bounds.add(_leftWall);
+		_bounds.add(_rightWall);
+
+		add(_bounds);
+
+		FlxG.worldBounds.set(-30, -30, FlxG.width + 60, FlxG.height + 60);
 	}
 	
 	/**
@@ -66,5 +93,25 @@ class PlayState extends FlxState
 	override public function update():Void
 	{
 		super.update();
+
+		FlxG.collide(_player, _bounds);
+
+		if (FlxG.keys.anyPressed(["LEFT", "A"]))
+		{
+			_player.velocity.x = -_playerSpeed;
+		} 
+
+		if (FlxG.keys.anyPressed(["RIGHT", "D"]))
+		{
+			_player.velocity.x = _playerSpeed;
+		} 
+
+		if (FlxG.keys.anyPressed(["Z", "SPACE", "UP", "W"]))
+		{
+			if (_player.isTouching(FlxObject.FLOOR)) 
+			{
+				_player.velocity.y = -_playerJump;
+			}	
+		}
 	}	
 }
