35d4fd86912fdfbfa9351117a321da8d4e2c63eb
Zach Latta <zach@zachlatta.com>
2014-04-25 22:37:53 -0700

    Okay, game's done.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,6 +5,7 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
 import flixel.util.FlxMath;
 
 /**
@@ -12,11 +13,41 @@ import flixel.util.FlxMath;
  */
 class PlayState extends FlxState
 {
+	private var _surface:FlxSprite;
+	private var _player:FlxSprite;
+	private var _background:FlxSprite;
+
+	private var _playerSpeed:Float = 120;
+	private var _playerJump:Float = 200;
+	private var _flagWalking:Bool = false;
+
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
+		FlxG.mouse.visible = false;
+		FlxG.cameras.flash(FlxColor.BLACK);
+
+		_background = new FlxSprite();
+		_background.loadGraphic("assets/images/background.png");
+		add(_background);
+
+		_player = new FlxSprite(132, 142);
+		_player.loadGraphic("assets/images/player.png", true, true, 71, 132);
+		_player.animation.add("idle", [0, 1], 4, true);
+		_player.x = 284;
+		_player.y = 300;
+		add(_player);
+
+		_player.animation.play("idle");
+
+		_surface = new FlxSprite();
+		_surface.loadGraphic("assets/images/surface.png");
+		_surface.x = 70;
+		_surface.y = 25;
+		add(_surface);
+
 		super.create();
 	}
 	
